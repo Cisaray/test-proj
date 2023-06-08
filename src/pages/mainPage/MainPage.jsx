c6d4f6eccd846255681136d958254f6a42991e1e
Cisaray <lyadovkzst@gmail.com>
2023-06-08 20:37:06 +0500

    main page is fully ready

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,6 +1,52 @@
 import style from './main.module.css'
-import {Link} from "react-router-dom";
+import { useNavigate} from "react-router-dom";
+import {useForm} from "react-hook-form";
+import React from "react";
+import InputMask from 'react-input-mask'
+import {useDispatch, useSelector} from "react-redux";
+import {setEmail, setPhoneNumber} from "../../redux/slices/formSlice";
+
 export const MainPage = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const {phone_number, email} = useSelector(state => state.form)
+
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm()
+  const formatPhoneNumber = (value) => {
+    const phoneNumber = value.replace(/\D/g, ''); // Удаление всех символов, кроме цифр
+
+    if (phoneNumber.length <= 3) {
+      return phoneNumber;
+    } else if (phoneNumber.length <= 6) {
+      return `+7 (${phoneNumber.slice(1, 4)}) ${phoneNumber.slice(4)}`;
+    } else if (phoneNumber.length <= 8) {
+      return `+7 (${phoneNumber.slice(1, 4)}) ${phoneNumber.slice(4, 7)}-${phoneNumber.slice(7)}`;
+    } else {
+      return `+7 (${phoneNumber.slice(1, 4)}) ${phoneNumber.slice(4, 7)}-${phoneNumber.slice(7, 9)}-${phoneNumber.slice(9, 11)}`;
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+']
+
+    if (!numbers.includes((e.key))) {
+      e.preventDefault()
+    }
+  }
+
+  const onSubmit = (data, e) => {
+    e.preventDefault()
+    dispatch(setEmail(data.email))
+    dispatch(setPhoneNumber(data.phone_number))
+    navigate('/create')
+    console.log(data)
+
+  }
+
   return (
     <div className={style.container}>
       <header className={style.header}>
@@ -23,25 +69,60 @@ export const MainPage = () => {
           </div>
         </div>
       </header>
-      <main className={style.content}>
+      <form onSubmit={handleSubmit(onSubmit)} className={style.content}>
+        <div>
+          <label className={style.input_label}>Номер телефона</label>
           <div>
-            <label className={style.input_label}>Номер телефона</label>
-            <div>
-              <input className={style.form_input} placeholder='+7 999 999-99-99' type="tel"/>
-            </div>
+            <InputMask
+              className={style.form_input}
+              mask='+7 (999) 999-99-99'
+              maskChar={null}
+              onKeyPress={handleKeyDown}
+              placeholder='+7 999 999 99 99'
+              name='phone_number'
+              defaultValue={phone_number}
+              {...register('phone_number',
+                {
+                  required: 'Это поле обязательное',
+                  pattern: {
+                    value: /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/,
+                    message: 'Неверный формат номера телефона',
+
+                  },
+                })}
+              onBlur={(e) => {
+                e.target.value = formatPhoneNumber(e.target.value)
+              }}
+            />
+            {errors.phone_number && <p className={style.phone_error}>{errors.phone_number.message}</p>}
+
           </div>
-          <div className={style.input_email_block}>
-            <label className={style.input_label}>Email</label>
-            <div>
-              <input className={style.form_input} placeholder='tim.jennings@example.com' type="email"/>
-            </div>
-          </div>
-        <Link to='/create'>
+        </div>
+        <div className={style.input_email_block}>
+          <label className={style.input_label}>Email</label>
           <div>
-            <button className={style.form_button}>Начать</button>
+            <input
+              className={style.form_input}
+              name='email'
+              defaultValue={email}
+              {...register('email',
+                {
+                  required: 'Это поле обязательное',
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Неправильный формат email',
+                  },
+                })}
+
+              placeholder='tim.jennings@example.com'
+              type="email"/>
+            {errors.email && <p className={style.phone_error}>{errors.email.message}</p>}
           </div>
-        </Link>
-      </main>
+        </div>
+
+          <button type='submit' className={style.form_button}>Начать</button>
+
+      </form>
     </div>
   )
 }
---
+++ src/redux/slices/formSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState ={
+  email: '',
+  phone_number: '',
+  firstPage:{},
+  secondPage:{},
+  finalPage:{}
+}
+
+const FormSlice = createSlice({
+  name: 'form',
+  initialState,
+  reducers:{
+    setEmail(state, action) {
+      state.email = action.payload
+    },
+    setPhoneNumber(state, action) {
+      state.phone_number=action.payload
+    }
+  }
+})
+
+export const {setPhoneNumber, setEmail} = FormSlice.actions
+export default FormSlice.reducer
